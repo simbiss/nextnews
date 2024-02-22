5aeabf72590d69c817f43eabd60d09d433d7fecd
Simon <simonbiss20@gmail.com>
2024-02-22 16:01:54 -0500

    implementation frontend pour la page a propos

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,10 +2,11 @@ import Link from 'next/link';
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>À propos</h1>
-      <p>Ceci est une page à propos dans une application Next.js.</p>
-      <Link legacyBehavior href="/"><a>Accueil</a></Link>
+    <div className="container mx-auto px-4">
+      <h1 className="text-4xl font-bold text-center my-6">À propos</h1>
+      <p className="text-lg my-4">Ceci est une page à propos dans une application Next.js.</p>
+      <Link legacyBehavior href="/">
+        <a className="text-blue-500 hover:text-blue-700 transition duration-300">Accueil</a></Link>
     </div>
   );
 };
