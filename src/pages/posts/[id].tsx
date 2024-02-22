19bff66698ac95f87e78ee002f13261d3d121cbd
Simon <simonbiss20@gmail.com>
2024-02-22 16:04:05 -0500

    implementation frontend pour la page post dynamique

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -6,10 +6,11 @@ const PostPage = () => {
   const { id } = router.query;
 
   return (
-    <div>
-      <h1>Post {id}</h1>
-      <p>Bienvenue à un routage Dynamique, {id}.</p>
-      <Link legacyBehavior href="/"><a>Retour à l'accueil</a></Link>
+    <div className="container mx-auto px-4">
+      <h1 className="text-4xl font-bold text-center my-6">Post {id}</h1>
+      <p className="text-lg my-4">Bienvenue à un routage Dynamique, {id}.</p>
+      <Link legacyBehavior href="/">
+        <a className="text-blue-500 hover:text-blue-700 transition duration-300">Retour à l'accueil</a></Link>
     </div>
   );
 };
