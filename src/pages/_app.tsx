394d5f1ace93cfffaabe9282f1f8d931cfa398bd
Simon <simonbiss20@gmail.com>
2024-02-22 18:15:43 -0500

    réglage petit bug d'interfacage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css'; 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return( 
+  <>
+  <Head>
+  <link rel="icon" href="/favicon.ico" />
+  </Head>
+  <Component {...pageProps} />
+  </>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,14 +10,14 @@ const HomePage = () => {
 
   return (
     <div className="container mx-auto px-4">
-      <h1 className='text-4x1 font-bold text-center my-6'>Next.js Routing</h1>
+      <h1 className='text-4x1 font-bold text-center my-10'>Next.js Routing</h1>
       <h2 className="text-2xl font-semibold my-4">Routage Statique</h2>
-      <Link legacyBehavior href="/about"><a className="text-blue-500 hover:text-blue-700 transition duration-300">À propos</a></Link> |
+      <Link legacyBehavior href="/about"><a className="text-blue-500 hover:text-blue-700 transition duration-300">Direction Statique</a></Link> 
 
       <h2 className="text-2xl font-semibold my-4">Routage Dynamique</h2>
       <input
         type="text"
-        className="form-input mt-1 block w-full px-3 py-2"
+        className="form-input mt-1 block w-full px-3 py-2 text-black"
         value={inputValue}
         onChange={handleInputChange}
         placeholder='Entrez votre nom'
