7384b593ee94828f98ae11bd8cb95d9b3f937096
Simon <simonbiss20@gmail.com>
2024-02-22 15:58:56 -0500

    implementation tailwindcss pour la page acceuils

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,21 +9,22 @@ const HomePage = () => {
   };
 
   return (
-    <div>
-      <h1>Next.js Routing</h1>
-      <h2>Routage Statique</h2>
-      <Link legacyBehavior href="/about"><a>À propos</a></Link> |
+    <div className="container mx-auto px-4">
+      <h1 className='text-4x1 font-bold text-center my-6'>Next.js Routing</h1>
+      <h2 className="text-2xl font-semibold my-4">Routage Statique</h2>
+      <Link legacyBehavior href="/about"><a className="text-blue-500 hover:text-blue-700 transition duration-300">À propos</a></Link> |
 
-      <h2>Routage Dynamique</h2>
+      <h2 className="text-2xl font-semibold my-4">Routage Dynamique</h2>
       <input
         type="text"
+        className="form-input mt-1 block w-full px-3 py-2"
         value={inputValue}
         onChange={handleInputChange}
         placeholder='Entrez votre nom'
       />
       {inputValue && (
         <Link legacyBehavior href={`/posts/${inputValue}`}>
-          <button type='button'>Direction Dynamique</button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3">Direction Dynamique</button>
         </Link>
       )}
     </div>
