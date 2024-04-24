a356f7dfbf5f8e5d74fa6f0da0e262534b7a898e
Simon <simonbiss20@gmail.com>
2024-04-24 16:13:33 -0400

    modification bare recherche

--- src/pages/mapInfo.tsx
+++ src/pages/mapInfo.tsx
@@ -27,7 +27,7 @@ const MapComponent: React.FC<{ apiKey: string }> = ({ apiKey }) => {
 
 const MyMapComponent: React.FC = () => {
   // Configuration initiale de la carte
-  const center = { lat: 48.8566, lng: 2.3522 }; // Coordonnées de Paris
+  const center = { lat: 45.508888, lng: -73.561668 }; // Coordonnées de Paris
   const zoom = 12;
 
   return (
@@ -42,9 +42,16 @@ const MyMapComponent: React.FC = () => {
 const MapPage: React.FC = () => {
   return (
     <div>
-      <div className="fixed top-0 left-0 right-0 z-10 p-4">
-        <input className="p-2 w-full" type="text" placeholder="Recherchez des adresses ou des lieux" />
-      </div>
+<div className="fixed top-10 left-20 right-20 z-10 p-4">
+  <div className="flex items-center bg-white shadow-md rounded-lg overflow-hidden">
+    <input className="p-2 w-full text-black" type="text" placeholder="Recherchez des adresses ou des lieux" />
+    <button className="p-4 bg-gray-200 hover:bg-gray-300">
+      <svg className="h-4 w-4 text-gray-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+      </svg>
+    </button>
+  </div>
+</div>
       <MapComponent apiKey={process.env.NEXT_GOOGLE_MAPS_API_KEY as string} />
     </div>
   );
